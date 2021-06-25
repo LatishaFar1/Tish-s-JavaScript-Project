176d1db6e73b53764dd41d890aa7749643ac942d
tish <lfaroul14@gmail.com>
2021-06-24 20:22:03 -0400

    first commit/fetch

---
+++ script.js
@@ -0,0 +1,14 @@
+//getting API data
+
+const URL = "https://reststop.randomhouse.com/resources"
+
+function getBooks(){
+return fetch(URL,{
+method: "GET",
+headers: {
+    "Content-type": application/json
+},
+body: JSON.stringify(body)
+})
+.then(response => response.json())
+}
