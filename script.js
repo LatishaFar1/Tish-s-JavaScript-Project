70fa6d1c12362e7fa9684ee13c1b26dab00c8699
tish <lfaroul14@gmail.com>
2021-07-01 23:51:45 -0400

    added locally hosted JSON server

--- script.js
+++ script.js
@@ -10,10 +10,36 @@ return fetch(URL)
 .then(dataRes => {
   console.log(dataRes);
 }) 
-}
+};
+
 //getting data on Loki - fix me!
 
 
 const lokiInfo = document.querySelector("#tvshow")
+
+
+getTV()
+.then(data => {
+  const lokiSumm = data.summary
+  lokiInfo.innerHTML = summary
+})
+
+
+
+
+
+
+// maybe add search for your own show?
+
+const searchURL = `http://api.tvmaze.com/search/shows?q=${search}`;
+function searchTV(search){
+return fetch(URL)
+.then(res => {
+  return res.json()
+})
+.then(dataRes => {
+  console.log(dataRes);
+}) 
+};
 
 
